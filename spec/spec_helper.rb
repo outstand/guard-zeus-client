7c28a4f94701e83ead262478747253dbd1dd6d35
Ryan Schlesinger <ryan@instanceinc.com>
2012-10-04 15:33:54 -0700

    Use :focus as the rspec focus tag.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,8 +5,8 @@ Bundler.require(:default, :development)
 require 'guard/zeus-client'
 
 RSpec.configure do |config|
-  config.filter_run :focused => true
-  config.alias_example_to :fit, :focused => true
+  config.filter_run :focus => true
+  config.alias_example_to :fit, :focus => true
   config.run_all_when_everything_filtered = true
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.color_enabled = true
